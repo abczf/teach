da8664b4aac7c7400c5c7220312ff097555b8f8b
xieyufei <a1678029050@163.com>
2020-11-04 20:04:40 +0800

    teach

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Teach
                 </div>
 
                 <div class="links">
