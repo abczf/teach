8c0bec1393b7d795994d734bc9c3447fc5bf9970
zhangzilianga <2297760371@qq.com>
2020-11-06 11:30:03 +0800

    年后

---
+++ resources/views/index/public/right.blade.php
@@ -0,0 +1,14 @@
+<div class="rmbar">
+	<span class="barico qq" style="position:relative">
+	<div  class="showqq">
+	   <p>官方客服QQ:<br>335049335</p>
+	</div>
+	</span>
+    <span class="barico em" style="position:relative">
+	  <img src="images/num.png" width="75" class="showem">
+	</span>
+    <span class="barico wb" style="position:relative">
+	  <img src="images/wb.png" width="75" class="showwb">
+	</span>
+    <span class="barico top" id="top">置顶</span>
+</div>
