e7f4dae9214a581e6f50be435f98e1088f8f1734
xieyufei <a1678029050@163.com>
2020-11-13 10:55:43 +0800

    上传

--- resources/views/index/public/personal/left.blade.php
+++ resources/views/index/public/personal/left.blade.php
@@ -2,13 +2,13 @@
 	<div class="membhead">
     <div style="text-align:center;"><img src="/index/images/0-0.JPG" width="80" ></div>
     <div style="width:220px;text-align:center;">
-    <p class="membUpdate mine">某某某</p> 
-    <p class="membUpdate mine"><a href="mysetting.html">修改信息</a>&nbsp;|&nbsp;<a href="myrepassword.html">修改密码</a></p>
+    <p class="membUpdate mine">某某某</p>
+    <p class="membUpdate mine"><a href="mysetting.html">修改信息</a></p>
     <div class="clearh"></div>
     </div>
     </div>
     <div class="memb">
-   
+
     <ul>
     	<li class="currnav"><a class="mb1" href="mycourse.html">我的课程</a></li>
 		<li><a class="mb3" href="myask.html">我的问答</a></li>
@@ -16,10 +16,10 @@
 		<li><a class="mb12" href="myhomework.html">我的作业</a></li>
         <li><a class="mb2" href="training_list.html" target="_blank">我的题库</a></li>
    </ul>
-   
+
     </div>
 
-    
+
   </div>
 
 
